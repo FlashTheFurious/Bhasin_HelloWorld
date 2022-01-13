bafbedfe84a352c39d4440fbe42246436ac4f9ab
tarna <tarna@DESKTOP-B6GP4N8>
2022-01-12 22:40:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Bhasin_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tarnav Aryan Bhasin");
+            float a = 10;
+            float b = 2.5f;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+        }
+    }
+}
